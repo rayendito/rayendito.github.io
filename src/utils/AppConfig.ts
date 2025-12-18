0af91d88594ece2d23cade334f4d815654281396
rayendito <13519157@std.stei.itb.ac.id>
2025-12-19 01:47:03 +0400

    titles

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
+	site_name: 'Ryandito Diandaru Personal Page',
+	title: 'R. Diandaru',
+	description: 'Ryandito Diandaru Personal Page',
+	author: 'Ryandito Diandaru',
+	locale_region: 'en-US',
 	locale: 'en'
 };
